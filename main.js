294d0081d8d7b058c49478005d494259a1510b32
Pranjal Patel <patelpranjal139@gmail.com>
2023-08-27 16:46:52 +0530

    Updated

--- main.js
+++ main.js
@@ -1,22 +1,7 @@
-// const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Ahmedabad';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '9e81ffb5c7msh04605a26809d784p1dbb84jsn8b71c4eb2da9',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
-};
+const APIkey = "dfda56122d04af1b23790dd7d2ffc801";
 
-
-fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=ahmedabad',options)
-	.then(response =>response.json())
-	.then(response =>console.log(response))
-	.catch(err =>console.error(err))
-
-// try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.text();
-// 	console.log(result);
-// } catch (error) {
-// 	console.error(error);
-// }
+const searchCitites = async(Searchtext) => {
+	const response = await fetch('http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}');
+	return response.json;
+}
+	
